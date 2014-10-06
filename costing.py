cb4290be2ff09e496fa44567abb243e1ad71def0
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2014-10-06 12:19:29 -0700

    initial commit, toolset should work properly

---
+++ costing.py
@@ -0,0 +1,51 @@
+from geopy import *
+import pandas as pd
+
+def get_path_cost(geopath,layerDict):
+    costDetail = {}
+    costDetail['total'] =0.0
+    costDetail['CraneWalk'] = 0.0
+    costDetail['Crossing'] = 0.0
+    #get default cranewalk cost SHOULD BE IMPROVED
+    walkCost = layerDict[layerDict.keys()[0]]['cd']
+    for step in geopath:
+        if step['properties']['activity'] == 'CraneWalk':
+            step['properties']['cost'] = step['properties']['length']*walkCost
+            costDetail['CraneWalk'] = step['properties']['cost']+costDetail['total']
+        elif step['properties']['activity'] == 'Crossing':
+            step['properties']['cost'] = layerDict[step['properties']['crossing']]['cc']
+            costDetail['Crossing'] = step['properties']['cost']+costDetail['Crossing']
+    costDetail['total'] =  costDetail['CraneWalk']+ costDetail['Crossing']
+    return costDetail, geopath
+    
+def get_total_cost(tsp_sol,tpos,layerDict,pathDirectory,CostSurfacefn):
+    cost = {}
+    cost['total'] = 0.0
+    cost['CraneWalk'] = 0.0
+    cost['Crossing'] = 0.0
+    for edge in tsp_sol.edges():
+        geograph = shortestPath(CostSurfacefn,pathDirectory+'Path'+edge[0]+'_'+edge[1]+'.shp',tpos[edge[0]],tpos[edge[1]],layerDict)
+        costing, geograph = get_path_cost(geograph,layerDict)
+        tsp_sol.edge[edge[0]][edge[1]]['costing'] = costing
+        tsp_sol.edge[edge[0]][edge[1]]['geobj'] = geograph
+        tsp_sol.edge[edge[0]][edge[1]]['TotalCost'] = costing['total']
+        cost['CraneWalk'] =cost['CraneWalk']+ tsp_sol.edge[edge[0]][edge[1]]['costing']['CraneWalk']
+        cost['Crossing'] =cost['Crossing'] +  tsp_sol.edge[edge[0]][edge[1]]['costing']['Crossing']
+    cost['total'] = cost['CraneWalk'] + cost['Crossing']
+    return cost, tsp_sol
+
+def get_detailed_schedule(solution,erectionCost):
+    sequence = nx.topological_sort(solution)
+    schedule = []
+    for index in range(len(sequence)):
+        erection = {}
+        erection['activity'] = 'Erect Turbine ' + str(sequence[index])
+        erection['cost'] = erectionCost
+        schedule.append(erection)
+        if index < len(sequence)-1:
+            for path in solution[sequence[index]][sequence[index+1]]['geobj']:
+                schedule.append(path['properties'])
+    return schedule
+
+
+        
