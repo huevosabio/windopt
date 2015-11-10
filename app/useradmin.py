3da92f2e358b74d3953b265529812492d47c42f8
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2015-11-09 21:15:30 -0800

    added useradmin endpoints

--- app/useradmin.py
+++ app/useradmin.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, request, jsonify, g, url_for
 from app import app
 from errors import BadRequestException, UserException
-from dbmodel import User, auth
+from app.dbmodel import User, auth
 import cPickle
 
 
@@ -17,7 +17,7 @@ def create_user():
 	'''
 	user_data = request.json
 	# check that the request is well formed.
-	if 'username' not in user_data and 'password' not in user_data:
+	if 'username' not in user_data or 'password' not in user_data:
 		raise BadRequestException('Malformed request.')
 
 	# check that user does not exist
@@ -33,11 +33,30 @@ def create_user():
 @app.route('/api/users/<username>', methods = ['GET', 'PUT', 'DELETE'])
 @auth.login_required
 def get_or_update_user(username):
+	'''
+	GET:
+		Returns the user info (username)
+	PUT:
+		Updates user info (pwd)
+	DELETE:
+		Deletes user
+	'''
 	try:
 		user = User.objects.get(username = username)
 	except User.DoesNotExist:
 		raise UserException('User does not exist.')
 	if request.method == 'GET':
 		return jsonify(username = user.username, message = 'User retrieved successfully')
-	elif request.method == 'PUT':
-		user.
+	elif request.method == 'PUT' and g.user.username == username:
+		# Method should be used only for changing password
+		try:
+			user.hash_password(request.json['password'])
+			user.save()
+			return jsonify(message = 'Password updated successfully.')
+		except:
+			raise BadRequestException('There was an error in the request.')
+	elif request.method == 'DELETE' and auth.username == username:
+		user.delete()
+		return jsonify(message = 'User deleted.')
+	else:
+		raise BadRequestException('Your request was not understood by the server.')
