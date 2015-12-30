c8f458dac955769917910f271aebc2d374901c0a
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2015-12-29 19:03:00 -0800

    added xeditable

--- gui/app/scripts/controllers/costs.js
+++ gui/app/scripts/controllers/costs.js
@@ -8,10 +8,35 @@
  * Controller of the windopsApp
  */
 angular.module('windopsApp')
-  .controller('CostsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CostsCtrl', function ($scope, $alert, cost) {
+  	$scope.newCostName = null;
+
+  	
+  	$scope.listCosts = function () {
+  		cost.listCosts().then( function(data){
+  			$scope.costs = data.costs;
+  		}).then(function(){
+  			//console.log($scope.costs);
+  		});
+  	}
+
+  	$scope.listCosts();
+
+  	$scope.createCost = function () {
+  		if ($scope.newCostName === null) {
+  			$alert({
+              content: 'Please name the new cost.',
+              animation: 'fadeZoomFadeDown',
+              type: 'danger',
+              duration: 3,
+              placement:'top-right'
+            });
+            return;
+  		}
+  		cost.createCost( $scope.newCostName ).then( function(data) {
+  			$scope.listCosts();
+  			$scope.newCostName = null;
+  		});
+  	}
+
   });
